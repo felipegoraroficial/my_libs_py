d60fb5dd4078f27db9b5d4af7d8babc8a30f9b07
felipegoraroficial <felipegoraro@outlook.com.br>
2026-01-17 13:27:51 -0300

    release: preparando versão v.1.0

--- app/__init__.py
+++
@@ -1,1 +0,0 @@
-from .commons import *  # noqa: F401,F403
--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-with open("README.md", "r") as arq:
+with open("README.md", "r", encoding="utf-8") as arq:
     readme = arq.read()
 
 setup(
     name="my-libs-py",
-    version="0.0.10",
-    license="MIT License",
+    version="0.0.11",
+    license="MIT",
     author="Felipe Pegoraro",
+    author_email="felipepegoraro93@gmail.com",
+    description="Commons utilitários para projetos Spark",
     long_description=readme,
     long_description_content_type="text/markdown",
-    author_email="felipepegoraro93@gmail.com",
-    keywords="libs python",
-    description="Wrapper para fins de estudo e cases de projetos pessoais",
-    packages=["my_libs"],
-    install_requires=["pyspark", "delta-spark"],
+    keywords="spark pyspark utils commons",
+    packages=find_packages(include=["my_libs", "app", "app.*"]),
+    install_requires=[
+        "pyspark>=4.0.0",
+        "delta-spark>=4.0.1",
+        "loguru>=0.7.3",
+        "holidays>=0.88",
+        "pandas>=2.3.2",  # só se o commons realmente usa
+        "pyarrow>=21.0.0",  # só se o commons realmente usa
+    ],
 )

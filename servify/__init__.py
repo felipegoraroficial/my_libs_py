82678a62dfebbf60282371f75287533abc3268b4
felipegoraroficial <felipegoraro@outlook.com.br>
2026-01-31 15:15:03 -0300

    release: preparando versão v.1.0 - organizando estrutura

--- servify/__init__.py
+++ servify/__init__.py
@@ -1,3 +1,3 @@
-from app.commons import reading_data  # noqa: F401,F403
+from app.commons import servify_read  # noqa: F401,F403
 
 __all__ = ["servify_read"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as arq:
 
 setup(
     name="servify",
-    version="0.0.12",
+    version="0.0.13",
     license="MIT",
     author="Felipe Pegoraro",
     author_email="felipepegoraro93@gmail.com",

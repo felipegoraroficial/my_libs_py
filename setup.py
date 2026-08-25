7926ad1e259ef76effa76375d107ff96b33f8e0c
felipegoraroficial <felipegoraro@outlook.com.br>
2026-08-25 15:52:15 -0300

    versão com read apenas release 1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as arq:
 
 setup(
     name="servify",
-    version="0.1.2",
+    version="0.1.3",
     license="MIT",
     author="Felipe Pegoraro",
     author_email="felipepegoraro93@gmail.com",
